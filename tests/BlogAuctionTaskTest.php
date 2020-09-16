7948f306222638fbecce8a38e5e50d846e0b534a
Fran Iglesias <franiglesias@mac.com>
2020-09-17 00:05:50 +0200

    Refactor BlogAuctionTaskTest to be more expressive

--- tests/BlogAuctionTaskTest.php
+++ tests/BlogAuctionTaskTest.php
@@ -2,27 +2,53 @@
 
 namespace Quotebot;
 
+use Generator;
 use PHPUnit\Framework\TestCase;
 use Quotebot\Domain\ProposalPublisher;
 
 class BlogAuctionTaskTest extends TestCase
 {
 
-    public function testShouldSendAProposal(): void
-    {
-        $marketStudyVendor = $this->createMock(\MarketStudyVendor::class);
-        $marketStudyVendor
-            ->method('averagePrice')
-            ->willReturn(0);
+    private $marketStudyVendor;
+    private $proposalPublisher;
 
-        $proposalPublisher = $this->createMock(ProposalPublisher::class);
-        $proposalPublisher
+    protected function setUp()
+    {
+        $this->marketStudyVendor = $this->createMock(\MarketStudyVendor::class);
+        $this->proposalPublisher = $this->createMock(ProposalPublisher::class);
+    }
+
+    /** @dataProvider casesProvider */
+    public function testShouldSendAProposal($averagePrice, $mode, $proposal): void
+    {
+        $this->givenAnAveragePrice($averagePrice);
+        $this->thenAProposalIsSentOf($proposal);
+        $this->whenIsPricedWIthMode($mode);
+    }
+
+    public function casesProvider(): Generator
+    {
+        yield 'Odd path basic calculation' =>  [0, 'SLOW', 6.28];
+    }
+
+    protected function givenAnAveragePrice($averagePrice): void
+    {
+        $this->marketStudyVendor
+            ->method('averagePrice')
+            ->willReturn($averagePrice);
+    }
+
+    protected function thenAProposalIsSentOf($proposal): void
+    {
+        $this->proposalPublisher
             ->expects(self::once())
             ->method('publish')
-            ->with(6.28);
+            ->with($proposal);
+    }
 
-        $blogAuctionTask = new BlogAuctionTask($marketStudyVendor, $proposalPublisher);
-        $blogAuctionTask->priceAndPublish('blog', 'SLOW');
-
+    protected function whenIsPricedWIthMode($mode): void
+    {
+        $blogAuctionTask = new BlogAuctionTask($this->marketStudyVendor, $this->proposalPublisher);
+        $blogAuctionTask->priceAndPublish('blog', $mode);
     }
 }
