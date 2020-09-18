644b27300088a8cdf06d96bf2cce7d65ac3011c4
Fran Iglesias <franiglesias@mac.com>
2020-09-18 20:03:00 +0200

    Test Odd proposal calculation

--- tests/BlogAuctionTaskTest.php
+++ tests/BlogAuctionTaskTest.php
@@ -31,14 +31,16 @@ class BlogAuctionTaskTest extends TestCase
     /** @dataProvider casesProvider */
     public function testShouldSendAProposal($averagePrice, $mode, $proposal): void
     {
+        $this->givenTimeIntervalIs(1);
         $this->givenAnAveragePrice($averagePrice);
         $this->thenAProposalIsSentOf($proposal);
-        $this->whenIsPricedWIthMode($mode);
+        $this->whenIsPricedWithMode($mode);
     }
 
     public function casesProvider(): Generator
     {
         yield 'Odd path basic calculation' =>  [0, 'SLOW', 6.28];
+        yield 'Even path basic calculation' => [1, 'SLOW', 6.30];
     }
 
     protected function givenAnAveragePrice($averagePrice): void
@@ -56,8 +58,13 @@ class BlogAuctionTaskTest extends TestCase
             ->with($proposal);
     }
 
-    protected function whenIsPricedWIthMode($mode): void
+    protected function whenIsPricedWithMode($mode): void
     {
         $this->blogAuctionTask->priceAndPublish('blog', $mode);
+    }
+
+    private function givenTimeIntervalIs($interval): void
+    {
+        $this->timeService->method('timeInterval')->willReturn($interval);
     }
 }
