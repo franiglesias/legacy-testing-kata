584c31746ab901615f5f46504f8cbc3ee579f80a
Fran Iglesias <franiglesias@mac.com>
2021-11-12 12:14:29 +0100

    Support for .env files and separate execution environments

--- src/Infrastructure/EntryPoint/Application.php
+++ src/Infrastructure/EntryPoint/Application.php
@@ -3,15 +3,23 @@
 namespace Quotebot\Infrastructure\EntryPoint;
 
 use Quotebot\AutomaticQuoteBot;
+use Symfony\Component\Dotenv\Dotenv;
 
 class Application
 {
     /** main application method */
     public static function main(array $args = null)
     {
-        $args = $args ?? ['APP_ENV' => 'prod'];
+        $path = $args['BASE_PATH'] ?? __DIR__ . '/../../../';
+        $dotEnvFile = $path . '.env';
 
-        if ($args['APP_ENV'] === 'prod') {
+        if (file_exists($dotEnvFile)) {
+            (new Dotenv())->loadEnv($dotEnvFile, 'APP_ENV');
+        }
+
+        $environment = $args['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+
+        if ($environment === 'prod') {
             $bot = new AutomaticQuoteBot();
             $bot->sendAllQuotes('FAST');
         }
--- tests/Infrastructure/EntryPoint/ApplicationTest.php
+++ tests/Infrastructure/EntryPoint/ApplicationTest.php
@@ -3,8 +3,9 @@ declare (strict_types=1);
 
 namespace Quotebot\Tests\Infrastructure\EntryPoint;
 
-use Quotebot\Infrastructure\EntryPoint\Application;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
+use Quotebot\Infrastructure\EntryPoint\Application;
 
 class ApplicationTest extends TestCase
 {
@@ -12,9 +13,23 @@ class ApplicationTest extends TestCase
      * @test
      * @doesNotPerformAssertions
      */
-    public function shouldRunWithoutCallingProductionVendors(): void
+    public function shouldRunInTestWithoutCallingProductionVendors(): void
     {
         Application::main(['APP_ENV' => 'test']);
+    }
+
+    /**
+     * @test
+     * @doesNotPerformAssertions
+     */
+    public function shouldRunDefinedEnvironment(): void
+    {
+        vfsStream::setup('quotebot', null, [
+            '.env' => 'APP_ENV=dev'
+        ]);
+        $path = vfsStream::url('quotebot/');
+
+        Application::main(['BASE_PATH' => $path]);
     }
 
 }
